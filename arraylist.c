a2d090408e802c3590a37b00b12a7024e7488f3f
Makman2 <mak95@arcor.de>
2014-01-12 01:07:42 +0100

    A managed array with list features

---
+++ arraylist.c
@@ -0,0 +1,179 @@
+#include "arraylist.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// Creates and sets up a new array list.
+struct ArrayList ArrayList_New(size_t typesize)
+{
+	struct ArrayList result;
+	
+	result.typesize = typesize;
+	result.count = 0;
+	// Reserve memory at least for one item because of performance reasons.
+	result.items = malloc(typesize);
+	
+	return result;
+}
+
+// Safely releases the list.
+void ArrayList_Release(struct ArrayList* list)
+{
+	// Only free memory.
+	free(list->items);
+}
+
+// Clears all items from the stack.
+void ArrayList_Clear(struct ArrayList* list)
+{
+	// Reallocate memory and reset count.
+	list->items = realloc(list->items, list->typesize);
+	list->count = 0;
+}
+
+// Adds an item at the end of the list.
+void* ArrayList_Add(struct ArrayList*, void* item)
+{
+	// Realloc and copy.
+	list->count++;
+	list->items = realloc(list->items, list->count * list->typesize);
+	memcpy((char*)list->items + list->typesize * (list->count - 1), item, list->typesize);
+	return (char*)list->items + list->typesize * (list->count - 1)
+}
+
+// Adds a range of items at the end of the list.
+void* ArrayList_AddRange(struct ArrayList*, void* items, int itemscount)
+{
+	if (itemscount <= 0)
+		return NULL;
+
+	list->count += itemscount;
+	list->items = realloc(list->items, list->count * list->typesize);
+	memcpy((char*)list->items + list->typesize * (list->count - itemscount), items, list->typesize * itemscount);
+	return (char*)list->items + list->typesize * (list->count - itemscount);
+}
+
+// Inserts an item at the specified index.
+void* ArrayList_Insert(struct ArrayList* list, void* item, int index)
+{
+	// If index out of range, exit.
+	if (index < 0 || index >= list->count)
+		return NULL;
+
+	// Realloc, copy behind and copy element.
+	list->count++;
+	list->items = realloc(list->items, list->count * list->typesize);
+	memmove((char*)list->items + list->typesize * (index + 1), (char*)list->items + list->typesize * index, list->typesize * (list->count - index - 1));
+	memcpy((char*)list->items + list->typesize * index, item, list->typesize);
+	return (char*)list->items + list->typesize * index;
+}
+
+// Inserts a range of items at the specified index.
+void* ArrayList_InsertRange(struct ArrayList* list, void* items, int itemscount, int index)
+{
+	// If index out of range, exit.
+	if (index < 0 || index >= list->count || itemscount <= 0)
+		return NULL;
+
+	// Realloc, copy behind and copy element.
+	list->count += itemscount;
+	list->items = realloc(list->items, list->count * list->typesize);
+	memmove((char*)list->items + list->typesize * (index + itemscount), (char*)list->items + list->typesize * index, list->typesize * (list->count - index - 1));
+	memcpy((char*)list->items + list->typesize * index, items, list->typesize * itemscount);
+	return (char*)list->items + list->typesize * index;
+}
+
+// Removes the first item found.
+bool ArrayList_Remove(struct ArrayList* list, void* item)
+{
+	// Search, remove with copy-back.
+	for(int i = 0; i < list->count; i++)
+	{
+		if(memcmp(item, (char*)list->items + list->typesize * i, list->typesize))
+		{
+			// Found.
+			memmove((char*)list->items + list->typesize * index, (char*)list->items + list->typesize * (index + 1), list->typesize * (list->count - index - 1));
+			list->count--;
+			if (list->count == 0)
+				list->items = realloc(list->items, list->typesize);
+			else
+				list->items = realloc(list->items, list->count * list->typesize);
+			
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+// Removes an item at the specified index.
+bool ArrayList_RemoveAt(struct ArrayList*, int index)
+{
+	// If index out of range, exit.
+	if (index < 0 || index >= list->count)
+		return false;
+	
+	// Just remove with copy-back.
+	memmove((char*)list->items + list->typesize * index, (char*)list->items + list->typesize * (index + 1), list->typesize * (list->count - index - 1));
+	list->count--;
+	if (list->count == 0)
+		list->items = realloc(list->items, list->typesize);
+	else
+		list->items = realloc(list->items, list->count * list->typesize);
+			
+	return true;
+}
+
+// Removes the given number of elements in the list at the specified index.
+bool ArrayList_RemoveRangeAt(struct ArrayList* list, int index, int count)
+{
+	// If index out of range, exit.
+	if (index < 0 || index + count > list->count || count <= 0)
+		return false;
+	
+	// Just remove with copy-back.
+	memmove((char*)list->items + list->typesize * index, (char*)list->items + list->typesize * (index + count), list->typesize * (list->count - index - count - 1));
+	list->count -= count;
+	if (list->count == 0)
+		list->items = realloc(list->items, list->typesize);
+	else
+		list->items = realloc(list->items, list->count * list->typesize);
+			
+	return true;
+}
+
+// Returns the item at the specified index.
+void* ArrayList_At(struct ArrayList* list, int index)
+{
+	return (char*)list->items + list->typesize * index;
+}
+
+// Returns the index of the first specified item found.
+int ArrayList_Find(struct ArrayList* list, void* item)
+{
+	for (int i = 0; i < list->count; i++)
+	{
+		if (memcmp((char*)list->items + list->typesize * i, item, list->typesize))
+		{
+			// Found.
+			return i;			
+		}
+	}
+	
+	return -1;
+}
+
+// Returns the index of the first specified range found.
+int ArrayList_FindRange(struct ArrayList* list, void* items, int itemscount)
+{
+	for (int i = 0; i < list->count; i++)
+	{
+		if (memcmp((char*)list->items + list->typesize * i, item, list->typesize * itemscount))
+		{
+			// Found.
+			return i;			
+		}
+	}
+	
+	return -1;
+}
---
+++ arraylist.h
@@ -0,0 +1,102 @@
+#pragma once
+/* Copyright (C) 2014 Mischa Krüger, Ammar Al-Qaiser, Frank Zimdars, Gordon Kemsies
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef WIN32
+	#include <stdbool.h>
+#endif
+
+// A managed array.
+struct ArrayList
+{
+	// The size of the type. Needed for memory allocation.
+	size_t typesize;
+		
+	// Number of items in this list.
+	int count;
+	
+	// A pointer to the items of the list.
+	void* items;
+
+};
+
+
+// Creates and sets up a new array list.
+// typesize: The size of the type the list should contain.
+struct ArrayList ArrayList_New(size_t typesize);
+
+// Safely releases the list.
+void ArrayList_Release(struct ArrayList* list);
+
+// Clears all items from the stack.
+void ArrayList_Clear(struct ArrayList* list);
+
+// Adds an item at the end of the list.
+// Returns a pointer to the newly added item in the list.
+// item: A pointer to the item to add.
+void* ArrayList_Add(struct ArrayList*, void* item);
+
+// Adds a range of items at the end of the list.
+// items: A pointer to the items array to add.
+// itemscount: The number of items of the array to add.
+void* ArrayList_AddRange(struct ArrayList*, void* items, int itemscount);
+
+// Inserts an item at the specified index.
+// Returns a pointer to the newly insertet item in the list.
+// item: A pointer to the item to insert.
+// index: The index where to insert.
+void* ArrayList_Insert(struct ArrayList* list, void* item, int index);
+
+// Inserts a range of items at the specified index.
+// items: A pointer to the items array to insert.
+// itemscount: The number of items of the array to insert.
+// index: The index where to insert.
+void* ArrayList_InsertRange(struct ArrayList* list, void* items, int itemscount, int index)
+
+// Removes the first item found.
+// Returns 'true' on success, 'false' on fail.
+// item: A pointer to the item to remove.
+bool ArrayList_Remove(struct ArrayList* list, void* item);
+
+// Removes an item at the specified index.
+// Returns 'true' on success, 'false' on fail.
+// index: The index of the element to remove.
+bool ArrayList_RemoveAt(struct ArrayList*, int index);
+
+// Removes the given number of elements in the list at the specified index.
+// Returns 'true' on success, 'false' on fail.
+// index: The index where the removing begins.
+// count: The number of items to remove from list.
+bool ArrayList_RemoveRangeAt(struct ArrayList* list, int index, int count);
+
+// Returns the item at the specified index.
+// index: The index of the item to get.
+void* ArrayList_At(struct ArrayList* list, int index);
+
+// Returns the index of the first specified item found.
+// item: The item to find.
+int ArrayList_Find(struct ArrayList* list, void* item);
+
+// Returns the index of the first specified range found.
+// items: The item range to find.
+// itemscount: The length of the item range.
+int ArrayList_FindRange(struct ArrayList* list, void* items, int itemscount);
