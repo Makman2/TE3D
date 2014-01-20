6eab19dc8b0038f6e648edb24a1689303b59049a
alqaiser22 <ammar-alqaiser92@web.de>
2014-01-20 03:54:33 +0100

    vpoints
    Konstruktorfehler beseitigt...

--- ascii_converter.c
+++ ascii_converter.c
@@ -5,21 +5,13 @@
 #include <stdlib.h>
 
 
-
-/*
-Ich habe die Datei mal etwas brauchbarer Struckturiert, ich hoffe es ist alles noch brauchbar.
-Siehe Kommentar unten
-
-*/
-
-
 // Converts vectors to an ASCII-art-representation and writes them to the target TE3D_Surface.
 bool TE3D_ASCII_Convert(struct TE3D_Vector4f vectors[], int count, struct TE3D_Surface* target, void* indices, enum TE3D_VectorFormat format, float* zBuffer)
 {
 	// If z-Buffer is NULL, create own one.
 	if (!zBuffer)
 		zBuffer = (float*)malloc(target->Width * target->Height);
-
+		
 	// -- Fragt ab, welches Format für die Verbindung der Vektoren gewählt wurde.
 	switch (format)
 	{
@@ -29,94 +21,69 @@ bool TE3D_ASCII_Convert(struct TE3D_Vector4f vectors[], int count, struct TE3D_S
 			// But if indices is not NULL, something may went not right, so exit function.
 			if (indices != NULL)
 				return false;
-
+			
 			/*( Hier gehts weiter mit dem Zeichnen. Die if-Bedingung von davor ist nur dazu da, um zu überprüfen ob indices nicht gesetzt wurden,
 			 ansonsten ist wohl im Funktionsaufruf was schiefgegangen. Aber falls nicht, sind wir eben an dieser Stelle. )
 			 Den Speicher für das TE3D_ColorChar Array von der Surface brauchst du nicht mit malloc zu initialisieren, das sollte der Benutzer der Funktion
 			 vorher selber machen.)*/
 
 
-// -- Gezeichnet wird in 'target'. Die Definition der Struktur TE3D_Surface ist in graphics.h zu finden.
 
+
+
+			
+// -- Gezeichnet wird in 'target'. Die Definition der Struktur TE3D_Surface ist in graphics.h zu finden.
+			
 // -- TE3D_Surface enthält Breite (Width) und Höhe (Height) und ein Array von TE3D_ColorChar's, die eben die Farbe und den jeweiligen Char enthalten.
 
 // -- Also im Prinzip ein Pixel vom Terminalbild. Dieses Array gilt es zu bauen aus den eingegebenen Vektoren (vectors).
-
+			
 // -- Zur Erinnerung: Die umzuwandelnden Vektoren nimmst du aus dem Parameter vectors[].
-
+			
 // -- Hier werden die Vektoren in Zeichen umgewandelt, und in diesem fall (also case ...POINTS) werden sie in Punkte umgezeichnet.
-
-
-			break;
-
-		// -- Hier wollen wir Linien zwischen den Vektoren zeichnen. Dazu sollte sich ein Array vom Typ TE3D_VectorIndex2 in 'indices' befinden.
-		case TE3D_VECTORFORMAT_LINES:
-			// Line connections.
-			// If indices is NULL the function must exit.
-			if (!indices)
-				return false;
-
-			// -- Also hier dasselbe mit dem Zeichnen, nur eben keine Punkte sondern Linien. Jetzt ist 'indices' nicht mehr NULL, sondern ein Zeiger auf
-			// -- ein Array von integern (also Ganzzahlen), die die Indexe beschreiben. D.h. Die Indexe sagen dir, welche Linien aus dem vectors-Array
-			// -- verbunden werden sollen.
-
-			break;
-
-		case TE3D_VECTORFORMAT_TRIANGLES:
-			// Triangle connections.
-			// If indices is again NULL the function must also exit.
-			if (!indices)
-				return false;
-
-			// -- Dasselbe hier wie oben nur jetzt mit Dreiecken malen und indices ist vom Typ Array von TE3D_VectorIndex3.
-
-			break;
-	}
-
-	// -- Wenn alles geklappt hat, 'true' zurückgeben.
-	// -- PS: Alle deutschen Kommentare (mit -- nach jedem //) werden am Ende gelöscht wenn die Funktion fertig ist.
-	return true;
-}
-
-
-/*
-
-    Worum handelt es sich bei den nachfolgenden Beschreibungen?
-    Ist die Struktur TE3D_Surface nicht schon in einer Headerdatei definiert?
-    Ein Semikolon darf innerhalb der funktionsbeschreibung nicht verwendet werden, oder soll dies in den Header?
-    Wo kommt die Variable Vectors her?
-    Die Funktion printf sollte in der fertigen Version möglichst nicht verwendet werden, da diese die Ausgabe überrscheibt. Scanf sollte nach möglichkeit auch
-    nicht umbedingt innerhalb der Bibliothek verwendet werden. (Falls es dennoch nötig ist scanf_s ?)
-
-    Dieser Kommentar kann gerne wieder entfernt werden,
-    Frank
-
-*/
 
 float vpoints (); {   // Fkt. mit x,y Koordinaten
 int Height;
 int Witdh;
 
-struct TE3D_Surface
-{
-	// A pointer to the array of colorized chars.
-	struct TE3D_ColorChar* Pixels;
-	// The width and height of the surface.
-	int Width;
-	int Height;
-	// The stride of the surface.
-	int Stride;
-}
 
+TE3D_Surface ( vponts (Height, Witdh));
 
-printf ("Geben Sie eine x und y Koordinate ein");
-scanf ("%d,%d");
-
-vectors[ Witdh, Height];
-
+target (vectors [int r]);
 
 
 
 return 0;
 			}
 
+
+			break;
+			
+		// -- Hier wollen wir Linien zwischen den Vektoren zeichnen. Dazu sollte sich ein Array vom Typ TE3D_VectorIndex2 in 'indices' befinden.
+		case TE3D_VECTORFORMAT_LINES:
+			// Line connections.
+			// If indices is NULL the function must exit.
+			if (!indices)
+				return false;
+			
+			// -- Also hier dasselbe mit dem Zeichnen, nur eben keine Punkte sondern Linien. Jetzt ist 'indices' nicht mehr NULL, sondern ein Zeiger auf
+			// -- ein Array von integern (also Ganzzahlen), die die Indexe beschreiben. D.h. Die Indexe sagen dir, welche Linien aus dem vectors-Array
+			// -- verbunden werden sollen.
+			
+			break;
+			
+		case TE3D_VECTORFORMAT_TRIANGLES:
+			// Triangle connections.
+			// If indices is again NULL the function must also exit.
+			if (!indices)
+				return false;
+				
+			// -- Dasselbe hier wie oben nur jetzt mit Dreiecken malen und indices ist vom Typ Array von TE3D_VectorIndex3.
+				
+			break;
+	}
+	
+	// -- Wenn alles geklappt hat, 'true' zurückgeben.
+	// -- PS: Alle deutschen Kommentare (mit -- nach jedem //) werden am Ende gelöscht wenn die Funktion fertig ist.
+	return true;
+}
