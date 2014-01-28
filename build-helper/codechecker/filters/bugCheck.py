ddf4e7bb2ac124940d0e30683733174b852a0942
Lasse Schuirmann <lasse@schuirmann.net>
2014-01-28 15:52:42 +0100

    Minor changes to code filters.

--- build-helper/codechecker/filters/bugCheck.py
+++ build-helper/codechecker/filters/bugCheck.py
@@ -2,6 +2,7 @@
 
 '''
 Copyright (C) 2013 Fabian Neuschmidt. All Rights Reserved.
+Copyright (C) 2014 Lasse Schuirmann. All Rights Reserved.
 Written by Fabian Neuschmidt (fabian.neuschmidt@googlemail.com)
 
 This program is free software: you can redistribute it and/or modify it under
@@ -11,20 +12,19 @@ version.
 '''
 import re
 
-def filter(queue,name,lineList):
-	"""returns error if 'BUG' is found in the file
 
-	:lineList: file as list of strings
-	:returns: all error messages in a list
+def filter(queue, name, lineList):
+    """returns error if 'BUG' is found in the file
 
-	"""
+    :lineList: file as list of strings
+    :returns: all error messages in a list
 
-	errorMessages=[name]
+    """
 
-	pattern = re.compile(r"\bBUG\b")
-	for lineNumberMinusOne, line in enumerate(lineList):
-		if pattern.search(line):
-#		if re.search(r"\bBUG\b", line):
-			errorMessages.append('"BUG" found in Line {}'.format(lineNumberMinusOne+1))
-#	return errorMessages
-	queue.put(errorMessages)
+    errorMessages = [name]
+
+    pattern = re.compile(r"\bBUG\b")
+    for lineNumberMinusOne, line in enumerate(lineList):
+        if pattern.search(line):
+            errorMessages.append('"BUG" found in Line {}'.format(lineNumberMinusOne + 1))
+    queue.put(errorMessages)
--- build-helper/codechecker/filters/fixmeCheck.py
+++ build-helper/codechecker/filters/fixmeCheck.py
@@ -2,6 +2,7 @@
 
 '''
 Copyright (C) 2013 Fabian Neuschmidt. All Rights Reserved.
+Copyright (C) 2014 Lasse Schuirmann. All Rights Reserved.
 Written by Fabian Neuschmidt (fabian.neuschmidt@googlemail.com)
 
 This program is free software: you can redistribute it and/or modify it under
@@ -11,21 +12,20 @@ version.
 '''
 import re
 
-def filter(queue,name,lineList):
-	"""returns error if 'FIXME' is found in the file
 
-	:lineList: file as list of strings
-	:returns: all error messages in a list
+def filter(queue, name, lineList):
+    """returns error if 'FIXME' is found in the file
 
-	"""
+    :lineList: file as list of strings
+    :returns: all error messages in a list
 
-	errorMessages=[name]
-	
-	pattern = re.compile(r"\bFIXME\b")	
-	for lineNumberMinusOne, line in enumerate(lineList):
-		if pattern.search(line):
-#		if re.search(r"\bFIXME\b", line):
-			errorMessages.append('"FIXME" found in Line {}'.format(lineNumberMinusOne+1))
-#	return errorMessages
-	queue.put(errorMessages)
+    """
+
+    errorMessages = [name]
+
+    pattern = re.compile(r"\bFIXME\b")
+    for lineNumberMinusOne, line in enumerate(lineList):
+        if pattern.search(line):
+            errorMessages.append('"FIXME" found in Line {}'.format(lineNumberMinusOne+1))
+    queue.put(errorMessages)
 
